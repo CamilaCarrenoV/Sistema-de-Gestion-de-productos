a7d6566df49f6dde81f49edccc1b2cce3333639d
CamilaCarrenoV <camila.carreno89@gmail.com>
2025-09-21 21:31:48 -0300

    First commit

---
+++ src/main/java/latinasincloud/Main.java
@@ -0,0 +1,26 @@
+package latinasincloud;
+
+/* SISTEMA DE GESTION DE PRODUCTOS
+Crear una aplicación de consola en Java que permita gestionar una lista de productos (nombre, precio, stock)
+con un metodo vender() para reducir el stock al realizar una venta.
+
+REQUISITOS
+ * Clase producto con:
+ - Atributos: id(int), nombre(string), precio(int), stock(int).
+ - Metodo vender(int cantidad): Reduce el stock y muestra un mensaje.
+
+ *Menú interactivo (Usando scanner) para:
+ - Añadir productos.
+ - Listar todos los productos.
+ - Vender productos (actualizando el stock).
+ - Salir del programa.
+
+ *Validaciones:
+ - No vender si no hay suficiente stock.
+ - Precio y stock no pueden ser negativos. */
+
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
